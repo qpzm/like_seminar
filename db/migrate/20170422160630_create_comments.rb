87ae6544f8bd8e6a20dec5b29da43b5c8c087365
김태완 <gimtaewan@gimtaewan-ui-MacBook-Pro.local>
2017-04-29 01:04:01 +0900

    test

--- db/migrate/20170422160630_create_comments.rb
+++ db/migrate/20170422160630_create_comments.rb
@@ -2,9 +2,11 @@ class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
       t.text :content
-      t.integer :post_id, null: false, index: true
-      t.integer :user_id, null: false, index: true
+      t.integer :post_id
+      t.integer :user_id
       t.timestamps
     end
   end
 end
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -14,12 +14,10 @@ ActiveRecord::Schema.define(version: 20170425043227) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
-    t.integer  "post_id",    null: false
-    t.integer  "user_id",    null: false
+    t.integer  "post_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -36,7 +34,6 @@ ActiveRecord::Schema.define(version: 20170425043227) do
     t.integer  "user_id",    null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
